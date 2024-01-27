f66b63e53483b5b57d6c62bca320a6037984bce5
Damian Koski <damian.koski97@gmail.com>
2024-01-27 22:29:08 +0100

    Add authentication server

---
+++ src/services/ProjectHarbor.Identity/ProjectHarbor.Identity.WebApi/Models/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProjectHarbor.Identity.WebApi.Models;
+
+public class User : IdentityUser
+{
+}
---
+++ src/services/ProjectHarbor.Identity/ProjectHarbor.Identity.WebApi/Persistence/DatabaseContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjectHarbor.Identity.WebApi.Models;
+
+namespace ProjectHarbor.Identity.WebApi.Persistence;
+
+public class DatabaseContext(DbContextOptions options) : IdentityDbContext<User>(options);
