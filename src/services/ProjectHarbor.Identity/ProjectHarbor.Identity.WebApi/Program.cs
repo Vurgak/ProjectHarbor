641972d31775c81060b4b2620e1216504cb8e21c
Damian Koski <damian.koski97@gmail.com>
2024-01-28 00:20:04 +0100

    Fix identity server database

--- src/services/ProjectHarbor.Identity/ProjectHarbor.Identity.WebApi/Program.cs
+++ src/services/ProjectHarbor.Identity/ProjectHarbor.Identity.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ProjectHarbor.Identity.WebApi.Models;
 using ProjectHarbor.Identity.WebApi.Persistence;
 
@@ -9,14 +10,21 @@ builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
 	.AddIdentityCookies();
 builder.Services.AddAuthorizationBuilder();
 
+var databaseConnectionString = builder.Configuration.GetConnectionString("Database");
 builder.Services.AddDbContext<DatabaseContext>(
-	options => options.UseSqlServer("Database"));
+	options => options.UseSqlServer(databaseConnectionString));
 
 builder.Services.AddIdentityCore<User>()
 	.AddEntityFrameworkStores<DatabaseContext>()
 	.AddApiEndpoints();
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options => options.AddPolicy("AllowAll", builder => builder
+		.SetIsOriginAllowed(_ => true)
+		.AllowAnyMethod()
+		.AllowAnyHeader()
+		.AllowCredentials()));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,4 +45,22 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapIdentityApi<User>();
 
+app.UseCors("AllowAll");
+
+if (IsRunningInContainer())
+	MigrateDatabase(app);
+
 app.Run();
+
+static bool IsRunningInContainer()
+{
+	var isRunningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") ?? "false";
+	return bool.Parse(isRunningInContainer);
+}
+
+static void MigrateDatabase(WebApplication app)
+{
+	using var serviceScope = app.Services.CreateScope();
+	var databaseContext = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+	databaseContext.Database.Migrate();
+}
