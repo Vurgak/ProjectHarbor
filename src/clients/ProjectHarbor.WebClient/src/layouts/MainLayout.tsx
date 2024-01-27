21cce7e94f227ab2a8e66d2314acdd1b90964fae
Damian Koski <damian.koski97@gmail.com>
2024-01-27 16:27:27 +0100

    Add page footer

--- src/clients/ProjectHarbor.WebClient/src/layouts/MainLayout.tsx
+++ src/clients/ProjectHarbor.WebClient/src/layouts/MainLayout.tsx
@@ -9,6 +9,10 @@ export default function MainLayout() {
       <div className="flex flex-1 max-w-screen-xl w-full mx-auto p-4">
         <Outlet />
       </div>
+
+      <footer className="p-4 text-muted-foreground text-center">
+        &copy; Damian Koski. All rights reserved.
+      </footer>
     </div>
   )
 }
